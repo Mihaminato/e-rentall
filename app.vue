3c3dd734b035700aaa25d0ff4e947083c7785b81
prince-sowell <prince.julienh@gmail.com>
2025-08-04 16:00:08 +0300

    delete dependances

--- app.vue
+++ app.vue
@@ -3,17 +3,5 @@
   <NuxtLoadingIndicator color="#e2725b" :height="3" :throttle="0" />
   <NuxtLayout>
     <NuxtPage />
-    Stagewise Toolbar - Seulement en mode développement
-    <ClientOnly>
-      <StagewiseToolbar :config="config" />
-    </ClientOnly>
   </NuxtLayout>
 </template>
-
-<script setup lang="ts">
-  import { StagewiseToolbar, type ToolbarConfig } from '@stagewise/toolbar-vue'
-
-  const config: ToolbarConfig = {
-    plugins: []
-  }
-</script>
